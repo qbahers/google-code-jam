a99f4c68bc24382350a5b90dc9108abede29c165
Quentin Bahers <quentin.bahers@gmail.com>
2014-08-18 12:49:01 +0200

    Solve Bullseye

--- 2013/round-1a/Bullseye/Bullseye.java
+++ 2013/round-1a/Bullseye/Bullseye.java
@@ -1,3 +1,4 @@
+import java.math.*;
 import java.util.*;
 
 public class Bullseye {
@@ -9,14 +10,35 @@ public class Bullseye {
             long r = in.nextLong();
             long t = in.nextLong();
 
-            long paintNeeded = 0;
-            long k = 0;
-            while (paintNeeded <= t) {
-                paintNeeded += 2 * r + 4 * k + 1;
-                k++;
+            long hi = 1;
+
+            while (check(r, t, hi)) {
+                hi *= 2;
             }
-            long nbBlackRings = k - 1;
-            System.out.printf("Case #%d: %d\n", i, nbBlackRings);
+
+            long lo = hi / 2;
+
+            while ((hi - lo) > 1) {
+                long mid = (lo + hi) / 2;
+                if (check(r, t, mid)) {
+                    lo = mid;
+                }
+                else {
+                    hi = mid;
+                }    
+            }
+
+            System.out.printf("Case #%d: %d\n", i, lo);
         }
+    }
+
+    private static boolean check(long r, long t, long k) {
+        BigInteger a = BigInteger.valueOf(2*r);
+        BigInteger b = BigInteger.valueOf(2*k);
+        BigInteger c = BigInteger.valueOf(1);
+        BigInteger d = BigInteger.valueOf(k);
+        BigInteger e = BigInteger.valueOf(t);
+        BigInteger res = a.add(b).subtract(c).multiply(d);
+        return res.compareTo(e) <= 0;
     }
 }
